77947115ee33bbc45ad63dc331ed55cd9bd4f409
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 18:45:23 +0000

    quick sanity check

--- pretrainbert/scripts/preprocess.py
+++ pretrainbert/scripts/preprocess.py
@@ -20,6 +20,7 @@ def main(config : str):
 
     tokenizer = ElectraTokenizerFast.from_pretrained(model_id) if 'electra' in model_id else AutoTokenizer.from_pretrained(model_id)
     processor = StandardProcessor(dataset, tokenizer, **config) if process_type == 'std' else CustomProcessor(hf_dset=dataset, hf_tokenizer=tokenizer, **config)
+    logging.info("Using {processor.columns} columns")
     logging.info("Processing Dataset")
     dataset = processor.map(**map_config)
     dataset.save_to_disk(out_dir)
